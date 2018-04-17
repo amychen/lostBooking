d7aad98b5ff013e3f65c5228700b6f9b8905382c
woamuii <ac6325@nyu.edu>
2018-04-18 00:01:53 +0800

    fix lostBooking.py

--- lostBooking.py
+++ lostBooking.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from flask.ext.mysqldb import MySql
+from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
@@ -9,7 +9,12 @@ conn = pymysql.connect(host='localhost',
                        db='flight_ticket_system',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
+mysql = MySQL(app)
 
 @app.route("/")
 def init():
+    cur = mysql.connection.cursor()
     return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 5000, debug=True)

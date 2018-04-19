ab6a0e5ac9c297f54ca96ae2d97e5f2ba552c27e
woamuii <ac6325@nyu.edu>
2018-04-19 08:10:50 +0800

    moved files and connected mySQL

--- lostBooking.py
+++ lostBooking.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, url_for
-from flask.ext.mysqldb import MySQL
+from flask import Flask, render_template, request, session, url_for, redirect
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       port = 8889,
-                       password='root',
-                       db='flight_ticket_system',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
-mysql = MySQL(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'flight_ticket_system'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost:8889'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL()	
+mysql.init_app(app)
 
 @app.route("/")
-def init():
-    cur = mysql.connection.cursor()
+def home():
+	return render_template('index.html')
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/register")
+def register():
     return render_template('register.html')
 
-if __name__ == '__main__':
-    app.run('127.0.0.1', 5000, debug=True)
+if __name__ == "__main__":
+	app.run('127.0.0.1', 5000, debug = True)

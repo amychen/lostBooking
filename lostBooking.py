88b3e6c6453f9914fdff356872b9f84921afc913
woamuii <ac6325@nyu.edu>
2018-04-26 00:45:12 +0800

    created individual page connections

--- lostBooking.py
+++ lostBooking.py
@@ -51,83 +51,65 @@ def registerAuth(userType):
            passportCty = request.form["passportCty"]
            dateOfBirth = request.form['dateOfBirth']
            ins = 'INSERT INTO customer(email, name, password, building_number, street, city, state, phone_number, \
-	 				passport_number, passport_expiration, passport_country, date_of_birth) VALUES(%s, %s, %s, %s, %s, %s, %s, \
-	 				%s, %s, %s, %s, %s)'
-           cursor.execute(ins, (email, name, password, buildingNo, street, city, state, phoneNum, passportNo, passportExp, passportCty, dateOfBirth))
-           conn.commit()
-           cursor.close()
-           return render_template('index.html')
+          passport_number, passport_expiration, passport_country, date_of_birth) VALUES(%s, %s, %s, %s, %s, %s, %s, \
+          %s, %s, %s, %s, %s)'
+           cursor.execute(ins, (username, name, password, buildingNo, street, city, state, phoneNum, passportNo, 
+            passportExp, passportCty, dateOfBirth))
        elif userType == "booking_agent":
            booking_agent_id = request.form['bookingAgentId']
-           ins = 'INSERT INTO booking_agent (email, password, booking_agent_id) VALUES(%s, %s)'
+           ins = 'INSERT INTO booking_agent (email, password, booking_agent_id) VALUES(%s, %s, %s)'
            cursor.execute(ins, (username, password, booking_agent_id))
-           conn.commit()
-           cursor.close()
-           return render_template('index.html')
        else:
            fname = request.form['fname']
            lname = request.form['lname']
            dateOfBirth = request.form['dateOfBirth']
            airline_name = request.form['airline_name']
            ins = 'INSERT INTO airline_staff(email, password, first_name, last_name, date_of_birth, airline_name) \
-           	    VALUES(%s, %s, %s, %s, %s, %s)'
-           cursor.execute(ins, (email, password, fname, lname, dateOfBirth, airline_name))
-           conn.commit()
-           cursor.close()
-           return render_template('index.html')
+                VALUES(%s, %s, %s, %s, %s, %s)'
+           cursor.execute(ins, (username, password, fname, lname, dateOfBirth, airline_name))
+       conn.commit()
+       cursor.close()
+       return render_template('login.html')
 
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-	username = request.form['username']
-	password = request.form['password']
-	userType = request.form['usertype']
+  username = request.form['username']
+  password = request.form['password']
+  userType = request.form['usertype']
 
-	cursor = conn.cursor()
-	if (userType == "airline_staff"):
-		query = 'SELECT * FROM  airline_staff WHERE username = %s and password = %s'
-	else:
-		query = 'SELECT * FROM ' + userType + ' WHERE email = %s and password = %s'
+  cursor = conn.cursor()
+  if (userType == "airline_staff"):
+    query = 'SELECT * FROM  airline_staff WHERE username = %s and password = %s'
+  else:
+    query = 'SELECT * FROM ' + userType + ' WHERE email = %s and password = %s'
 
-	cursor.execute(query, (username, password))
-	data = cursor.fetchone()
-	cursor.close()
-	error = None
-	if (data == False):
-		error = 'Invalid login. Did you register?'
-		return render_template('login.html', error=error)
-	else: 
-		if (userType == "airline_staff"):
-			session['username'] = username
-		else:
-			session['email'] = username
-		return redirect(url_for('home'))
-			
+  cursor.execute(query, (username, password))
+  data = cursor.fetchone()
+  cursor.close()
+  error = None
+  if (data == False):
+    error = 'Invalid login. Did you register?'
+    return render_template('login.html', error=error)
+  else: 
+    session['username'] = username
+    session['type'] = userType
+    return redirect(url_for('home'))
 
 
 @app.route('/home')
 def home():
-	if (session['email'] != ""):
-		username = session['username']
-		print(username + "\n");
-		return render_template('staff.html')
-	else:
-		username = session['email']
-		cursor = conn.cursor()
-		query = 'SELECT * FROM customer WHERE username = %s'
-		cursor.execute(query, username)
-		data = cursor.fetchone()
-		cursor.close()
-		if (data):
-			return render_template('customer.html')
-		else:
-			return render_template('agent.html')
+  username = session['username']
+  user = session['type']
+  return render_template(user + '.html', username=username)
+
+
 
 @app.route('/logout')
 def logout():
-	session.pop('username')
-	return redirect('/')
+  session.pop('username')
+  return redirect('/')
 
 app.secret_key = 'b\'e3r\xd7\xf4\xc7g\xd7N\xf5\xefV\xb9\xdf\xed\xf2P%~\t\x8f.X\x91'
 
 if __name__ == "__main__":
-	app.run('127.0.0.1', 5000, debug = True)
+  app.run('127.0.0.1', 5000, debug = True)
